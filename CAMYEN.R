000cc17d68b037ef9d78163c5fecfac904814ec7
noahbcp <noah_piper@hotmail.com>
2022-06-29 00:33:58 +1000

    Rewrote entire script. Now intelligently selects triplicates - no longer need to specify wells. Better file saving behaviour.

--- CAMYEN.R
+++ CAMYEN.R
@@ -1,100 +1,182 @@
-{ 
-#-----Dependencies-----
-rm(list = ls())
-tidyverse_req <- require(tidyverse) 
-if (tidyverse_req == FALSE) {install.packages('tidyverse')}
+#----Dependencies----
+if (require(tidyverse) == FALSE) {
+    install.packages("tidyverse")
+    }
+if (require(tools) == FALSE) {
+    install.packages("tools")
+    }
+if (require(hablar) == FALSE) {
+    install.packages("hablar")
+    }
 library(tidyverse)
-tools_req <- require(tools) 
-if (tools_req == FALSE) {install.packages('tools')} 
 library(tools)
-hablar_req <- require(hablar)
-if(hablar_req == FALSE) {install.packages('hablar')}
 library(hablar)
-
-#-----Crayon Styles-----
-alert <- crayon::combine_styles('bold', 'red')
-greenlight <- crayon::combine_styles('bold', 'green')
-
-#-----Code-----
-#Select files
-data_path_folder <- as.character(readline(prompt = 'Enter the pathname of your data folder: ')) %>% file.path()
-data_path_files <- list_files_with_exts(data_path_folder, exts = 'csv')
-while (length(data_path_files) == 0) {
-    cat(alert('There are no `.csv` files in that folder.'))
-    data_path_folder <- as.character(readline(prompt = 'Enter the pathname of your data folder: ')) %>% file.path()
-    data_path_files <- list_files_with_exts(data_path_folder, exts = 'csv')
+options(readr.show_col_types = FALSE)
+#----Styles----
+styles <- new.env()
+styles$alert <- crayon::combine_styles("bold", "red")
+styles$greenlight <- crayon::combine_styles("bold", "green")
+#----Closures----
+filehandler <- local({
+    files <- c()
+    list(
+        fetch_files = function() {
+            files <<- list_files_with_exts(
+                file.path(
+                    readline("Enter the pathname of your data folder: ")),
+                exts = "csv")
+            while (length(files) == 0) {
+                cat(styles$alert("There are no `.csv` files in that location."))
+                files <<- list_files_with_exts(
+                    file.path(
+                        readline("Enter the pathname of your data folder: ")),
+                        exts = "csv")
+            }
+        },
+        filepaths = function() {
+            return(files)
+        },
+        basenames = function(silent = FALSE) {
+            file_basenames <- basename(files)
+            if (silent == FALSE) {
+                cat(styles$greenlight("The following files were found:\n"))
+                print(file_basenames)
+                return(file_basenames)
+            } else {
+                return(file_basenames)
+                }
+        },
+        save_files = function(x) {
+            #Creates a new directory in the parent directory
+            savepath <- file.path(paste0(dirname(files[batcher$file_integer()]), "/Processed"))
+            dir.create(savepath, showWarnings = FALSE)
+            savepath <- file.path(paste0(savepath, "/", filehandler$basenames(TRUE)[batcher$file_integer()]))
+            write_csv(x, savepath)
+        }
+    )
+})
+batcher <- local({
+    file_int <- c()
+    batch_process <- logical()
+    list(
+        offer_batch_process = function() {
+            if (length(filehandler$filepaths()) > 1) {
+                batch_prompt <- toupper(as.character(readline(prompt = "Batch process files? (Y/N): ")))
+                if (batch_prompt == "N") {
+                    batch_process <<- FALSE
+                    file_int <<- as.integer(readline(prompt = "Which file should be processed? (Enter the corresponding number): "))
+                    cat(styles$greenlight("The following file will be processed:\n"))
+                    print(basename(filehandler$filepaths()[file_int]))
+                } else {
+                    batch_process <<- TRUE
+                    file_int <<- 1
+                }
+            } else {
+                batch_process <<- FALSE
+                file_int <<- 1
+            }
+        },
+        batch_status = function() {
+            return(batch_process)
+        },
+        file_integer = function() {
+            return(file_int)
+        },
+        increment_file_integer = function(x) {
+            file_int <<- file_int + 1
+            return(file_int)
+        }
+    )
+})
+datahandler <- local({
+    data <- list()
+    pos_row <- c()
+    pos_col <- c()
+    n_cycles <- c()
+    list(
+        view_data = function(silent = FALSE) {
+            data <<- read_csv(
+                filehandler$filepaths()[batcher$file_integer()],
+                col_names = c(as.character(1:13)), #Rename columns
+                skip = 14) #Skip `.csv` header
+            if (silent == FALSE) {
+                View(data, filehandler$basenames(TRUE)[batcher$file_integer()])
+            }
+        },
+        fetch_exp = function(silent = FALSE) {
+            if (batcher$batch_status() == FALSE) {
+                pos_row <<- 3 #Assumes data begins in row 3.
+                pos_col <<- datahandler$find_triplicate()
+            } else {
+                pos_row <<- 3 #Assumes data begins in row 3.
+                pos_col <<- datahandler$find_triplicate()
+            }
+            if (silent == FALSE) {
+                n_cycles <<- as.numeric(readline("How many cycles?: "))
+            }
+        },
+        find_triplicate = function() {
+            #Gives a vector of the triplicate to be passed to `pos_col`
+            #[2] is called as the first col is the plate coordinate.
+            return(which(!is.na(data[3, 1:13]))[2]) 
+        },
+        row_position = function() {
+            return(pos_row)
+        },
+        column_position = function() {
+            return(pos_col)
+        },
+        calculate_bret = function(batch = FALSE) {
+            #Loop to fetch wavelength A
+            datalist_wavelength_A <- list()
+            i <- 1
+            pos_row_A <- pos_row
+            pos_col_A <- pos_col
+            while (i <= n_cycles){
+                wavelength_A <- data[(pos_row_A:(pos_row_A + 7)), (pos_col_A:(pos_col_A + 2))]
+                wavelength_A <- as.vector(t(wavelength_A))
+                pos_row_A <- (pos_row_A + 23)
+                datalist_wavelength_A[[i]] <- as.numeric(wavelength_A)
+                i <- i + 1
+            }
+            #Loop to fetch wavelength B
+            datalist_wavelength_B <- list()
+            i <- 1
+            pos_row_B <- pos_row
+            pos_col_B <- pos_col
+            while (i <= n_cycles){
+                wavelength_B<- data[((pos_row_B + 11):((pos_row_B + 7) + 11)), (pos_col_B:(pos_col_B + 2))]
+                wavelength_B <- as.vector(t(wavelength_B))
+                pos_row_B <- (pos_row_B + 23)
+                datalist_wavelength_B[[i]] <- as.numeric(wavelength_B)
+                i <- i + 1
+            }
+            #Calculate BRET ratio (wavelength A / wavelength B)
+            datalist <- list(1:n_cycles)
+            i <- 1
+            while (i <= n_cycles){
+                datalist[[i]] <- datalist_wavelength_A[[i]] / datalist_wavelength_B[[i]]
+                i <- i + 1
+            }
+            datalist <- as.data.frame(do.call(rbind, datalist))
+            return(datalist)
+        }
+    )
+})
+#----Script----
+filehandler$fetch_files()
+filehandler$basenames()
+batcher$offer_batch_process()
+datahandler$view_data()
+datahandler$fetch_exp()
+if (batcher$batch_status() == TRUE) {
+    while (batcher$file_integer() <= length(filehandler$filepaths())) {
+        datahandler$view_data(silent = TRUE)
+        datahandler$fetch_exp(silent = TRUE)
+        filehandler$save_files(datahandler$calculate_bret())
+        batcher$increment_file_integer()
+    }
+}  else {
+    filehandler$save_files(datahandler$calculate_bret())
 }
-print(data_path_files)
-#Offer batch Process
-if (length(data_path_files) > 1) {
-    file_prompt <- toupper(as.character(readline(prompt = 'Batch process files? (Y/N): ')))
-    if (file_prompt == 'N') {
-        batch.process <- FALSE
-        file_prompt <- as.integer(readline(prompt = 'Which file should be processed? (Enter the corresponding number): '))
-    } else {
-        file_prompt <- 1
-        batch.process <- TRUE
-    }
-} else {
-    batch.process <- FALSE
-    file_prompt <- 1
-    }
-#Ask user to define position of data in CSV and experiment setup
-data_path <- unlist(data_path_files[1])
-options(readr.show_col_types = FALSE) #Suppresses error message
-data <- read_csv(data_path_files[file_prompt], col_names = FALSE)
-View(data)
-pos_row <- as.integer(readline(prompt = 'Row # of first well in triplicate: '))
-pos_col <- as.integer(readline(prompt = 'Col # of first well in triplcate: '))
-n_cycles <- as.integer(readline(prompt = 'Number of cycles: '))
-drug <- as.character(readline(prompt = 'Compound (determines filename): '))
-#Begin batch loop
-batch.i <- 1
-while (batch.i <= length(data_path_files)) {
-if (batch.process == FALSE) {
-    data_path <- data_path_files[file_prompt]
-    data <- read_csv(data_path, col_names = FALSE)
-} else {
-    data_path <- data_path_files[batch.i]
-    data <- read_csv(data_path, col_names = FALSE)
-}
-    #Loop to fetch wavelength A
-    datalist_wavelength_A <- list(1:n_cycles)
-    i <- 1
-    pos_row_A <- pos_row
-    pos_col_A <- pos_col
-    while (i <= n_cycles){
-        wavelength_A <- data[(pos_row_A:(pos_row_A + 7)), (pos_col_A:(pos_col_A + 2))]
-        wavelength_A <- as.vector(t(wavelength_A))
-        pos_row_A <- (pos_row_A + 23)
-        datalist_wavelength_A[[i]] <- as.numeric(wavelength_A)
-        i <- i + 1
-    }
-    #Loop to fetch wavelength B
-    datalist_wavelength_B <- list(1:n_cycles)
-    i <- 1
-    pos_row_B <- pos_row
-    pos_col_B <- pos_col
-    while (i <= n_cycles){
-        wavelength_B<- data[((pos_row_B + 11):((pos_row_B + 7) + 11)), (pos_col_B:(pos_col_B + 2))]
-        wavelength_B <- as.vector(t(wavelength_B))
-        pos_row_B <- (pos_row_B + 23)
-        datalist_wavelength_B[[i]] <- as.numeric(wavelength_B)
-        i <- i + 1
-    }
-    #Calculate BRET ratio (wavelength A / wavelength B)
-    datalist <- list(1:n_cycles)
-    i <- 1
-    while (i <= n_cycles){
-        datalist[[i]] <- datalist_wavelength_A[[i]] / datalist_wavelength_B[[i]]
-        i <- i + 1
-    }
-    savepath <- paste0(stringr::str_replace_all(data_path, '.csv', ''), '_', drug, '_processed.csv')
-    datalist <- as.data.frame(do.call(rbind, datalist))
-    write_csv(datalist, savepath)
-    #Escapes batch loop if user does not opt for batch process
-    if (batch.process == TRUE) {
-        batch.i <- batch.i + 1
-    } else {batch.i <- (length(data_path_files) + 1)} #Have to increase var by length + 1 to escape batch loop
-}
-}
+cat(styles$greenlight('Export completed!'))
