ef47172749bc2fa79d17303d46d03288a24ceed9
noahbcp <noah_piper@hotmail.com>
2023-02-07 13:31:51 +1000

    Feat: Dependencies

---
+++ FDSS Calcium.R
@@ -0,0 +1,11 @@
+# Dependencies
+
+if (require(magrittr) == FALSE) {
+    install.packages('magritr')
+}
+if (require(openxlsx) == FALSE) {
+    install.packages('openxlsx')
+}
+if (require(stringr) == FALSE) {
+    install.packages('stringr')
+}
